9bc3d9d65791188a8b4e5bd7fc82bacd4b35db19
Kwaye Kant <gabrielkwaye@gmail.com>
2026-02-24 14:13:37 +0100

    Front end app init commit that works

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductList } from './components/product-list/product-list';
+import { ProductCard } from './components/product-card/product-card';
+import { ProductForm } from './components/product-form/product-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductList, ProductCard, ProductForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
